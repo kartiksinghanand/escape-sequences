ff3906bee06102e73443351566aea3279b5402bc
kartiksinghanand <34029157+kartiksinghanand@users.noreply.github.com>
2020-04-03 04:02:29 -0700

    Add files via upload

---
+++ escape_sequences.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    printf("this is the newline sequence, enter anything\n");
+    getch();
+    printf("Hello world\a\n");
+    getch();
+    printf("this is the alert sound\a\n");
+    getch();
+    printf("this is the \t tab sequence \n");
+    printf("this is the \"double quote sequence\"\a\n");
+    printf("this is the backspace \bsequence \n");
+    printf("this is the slash \\ sequence\n");
+    printf("this is the carriage return sequence my name is kartik \rsingh\n");
+    printf("this is the single \'single quote sequence\'\n");
+    printf("however, these single quotes can be generated without the use of escape sequences too: 'like this'\n");
+    printf("this is the vertical tab sequence \v");
+    printf("\nthis is the form feed \f sequence\n");
+    printf("this is the question mark sequence, what is you name \? \n");
+
+}
